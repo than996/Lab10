712049b12db4d29c1b8bb3cc93c3dd4015ed4d1b
Nathanael Butler <nlbutler@umich.edu>
2020-12-03 20:54:07 -0500

    Added Positive and Negative Tests

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -10,7 +10,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class testSelectionSort {
-	@Test
+
 	public void test() {
 		testPositive();
 		testNegative();
@@ -20,7 +20,8 @@ public class testSelectionSort {
 
     public testSelectionSort() {
     }
-    
+
+		@Test
     public void testPositive(){
         int[] arr = new int[5];
         arr[0] = 8;
@@ -28,29 +29,55 @@ public class testSelectionSort {
         arr[2] = 7;
         arr[3] = 10;
         arr[4] = 2;
-        
+
+        SelectionSort Sort = new SelectionSort();
+        int[] Sorted = Sort.basicSelectionSort(arr);
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = 2;
         Sortedarr[1] = 7;
         Sortedarr[2] = 8;
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
-        
-        /** add tests to check for this unit test **/
-    }
-    
-    public void testNegative(){
-        
-        /** Test data contains negative values only **/
-    }
-    
+
+        for(int i = 0; i < 5; i++)
+        {
+        	assertEquals("Result", Sortedarr[i], Sorted[i]);
+        }
+
+				@Test
+		    public void testNegative(){
+
+		        int[] arr = new int[5];
+		        arr[0] = -8;
+		        arr[1] = -9;
+		        arr[2] = -7;
+		        arr[3] = -10;
+		        arr[4] = -2;
+
+		        SelectionSort Sort = new SelectionSort();
+		        int[] Sorted = Sort.basicSelectionSort(arr);
+
+		        int[] Sortedarr = new int[5];
+		        Sortedarr[0] = -10;
+		        Sortedarr[1] = -9;
+		        Sortedarr[2] = -8;
+		        Sortedarr[3] = -7;
+		        Sortedarr[4] = -2;
+
+		        for(int i = 0; i < 5; i++)
+		        {
+		        	assertEquals("Result", Sortedarr[i], Sorted[i]);
+		        }
+		    }
+
     public void testMixed(){
-        
+
         /** Test data contains with both positive, negative and zeros **/
     }
-    
+
     public void testDuplicates(){
-        
+
         /** Test data contains duplicates **/
     }
 
